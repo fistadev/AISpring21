6ec2593958e8fc07693ea2e0e765694791f8cfc5
fistadev <fabiofistarol@gmail.com>
2021-05-28 12:13:33 +0200

    Update mnist.py

--- M5 - Deep Learning/D3 - MNIST/mnist.py
+++ M5 - Deep Learning/D3 - MNIST/mnist.py
@@ -2,7 +2,6 @@ from collections import OrderedDict
 
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 import torch
 from torch import nn
@@ -26,14 +25,15 @@ testset = datasets.MNIST("MNIST_data/", download=True, train=False, transform=tr
 testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=True)
 
 
-class Network(nn.Module):
+class NNetwork(nn.Module):
 
     # Defining the layers, 128, 64, 10 units each
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(16, 10)
+        self.fc1 = nn.Linear(784, 16)
+        self.fc2 = nn.Linear(16, 32)
+        self.fc3 = nn.Linear(32, 8)
+        self.fc4 = nn.Linear(8, 10)
 
     # Forward pass through the network, returns the output logits
     def forward(self, x):
@@ -42,45 +42,13 @@ class Network(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         x = self.fc3(x)
-        x = F.softmax(x, dim=1)
+        x = F.relu(x)
+        x = self.fc4(x)
         return x
 
 
-model = Network()
-print(model)
-
-
-# Hyperparameters for our network
-input_size = 784
-hidden_sizes = [4, 4]
-output_size = 10
-
-# Build a feed-forward network
-model = nn.Sequential(
-    nn.Linear(input_size, hidden_sizes[0]),
-    nn.ReLU(),
-    nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-    nn.ReLU(),
-    nn.Linear(hidden_sizes[1], output_size),
-    nn.Softmax(dim=1),
-)
-print(model)
-
-
-model = nn.Sequential(
-    OrderedDict(
-        [
-            ("fc1", nn.Linear(input_size, hidden_sizes[0])),
-            ("relu1", nn.ReLU()),
-            ("fc2", nn.Linear(hidden_sizes[0], hidden_sizes[1])),
-            ("relu2", nn.ReLU()),
-            ("output", nn.Linear(hidden_sizes[1], output_size)),
-            ("logits", nn.Linear(hidden_sizes[1], output_size)),
-            ("softmax", nn.Softmax(dim=1)),
-        ]
-    )
-)
-print(model)
+model = NNetwork()
+# print(model)
 
 
 # training the network
@@ -90,23 +58,7 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 # optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 
-print("Initial weights - ", model.fc1.weight)
-
-images, labels = next(iter(trainloader))
-images.resize_(16, 784)
-
-# Clear the gradients, do this because gradients are accumulated
-optimizer.zero_grad()
-
-# Forward pass, then backward pass, then update weights
-output = model.forward(images)
-loss = criterion(output, labels)
-loss.backward()
-print("Gradient -", model.fc1.weight.grad)
-optimizer.step()
-
-
-epochs = 3
+epochs = 5
 print_every = 40
 
 for e in range(epochs):
@@ -130,3 +82,47 @@ for e in range(epochs):
         if i % print_every == 0:
             print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
             running_loss = 0
+
+
+print("Initial weights - ", model.fc1.weight)
+
+images, labels = next(iter(trainloader))
+images.resize_(16, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model.forward(images)
+loss = criterion(output, labels)
+loss.backward()
+print("Gradient -", model.fc1.weight.grad)
+optimizer.step()
+
+
+correct_train = 0
+total_train = 0
+
+with torch.no_grad():
+    for data in trainset:
+        X, y = data
+        output1 = model.forward(X.view(-1, 784))
+        for idx, i in enumerate(output1):
+            if torch.argmax(i) == y[idx]:
+                correct_train += 1
+            total_train += 1
+
+correct_test = 0
+total_test = 0
+
+with torch.no_grad():
+    for data in testset:
+        X, y = data
+        output1 = model.forward(X.view(-1, 784))
+        for idx, i in enumerate(output1):
+            if torch.argmax(i) == y[idx]:
+                correct_test += 1
+            total_test += 1
+
+print(f"Accuracy on train set: {round((correct_train/total_train)*100, 3)}%")
+print(f"Accuracy on test set: {round((correct_test/total_test)*100, 3)}%")
