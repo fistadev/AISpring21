d3a9e37cfa4ab642986c0af657d65d715f6ae986
fistadev <fabiofistarol@gmail.com>
2021-06-10 20:37:49 +0200

    Update green_screen.py

--- M6 - Computer Vision/CV3 - Histograms, transformations and Bitwise ops/green_screen.py
+++ M6 - Computer Vision/CV3 - Histograms, transformations and Bitwise ops/green_screen.py
@@ -15,43 +15,51 @@ PATH = "bird.jpg"
 img = cv2.imread(PATH)
 rgb_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-imshow(rgb_img)
+# imshow(rgb_img)
 
 
 
-print(img.shape)
+
+# print(img.shape)
 
 
 # green screen
-green_screen = np.zeros((800,800), dtype="uint8")
-print(green_screen.shape)
+green_screen = cv2.imread('img/1.jpg')
+rgb_green = cv2.cvtColor(green_screen,cv2.COLOR_BGR2RGB)
+print(rgb_green.shape)
+# imshow(rgb_green)
+
+
+# mask
+
+
 
 
 # video
 
-video = cv2.VideoCapture(0)
-
-
-while video.isOpened():
-    check, frame = video.read()
-    if frame is not None:
-        # img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # img = frame
-        low_blue = np.array([110, 20, 0])
-        upper_blue = np.array([55, 255, 255])
-        mask_img = cv2.inRange(img, low_blue, upper_blue)
-        result = cv2.bitwise_and(img, img, mask=mask_img)
-        # img = frame
-        cv2.imshow("mask", mask_img)
-        cv2.imshow("original", img)
-        cv2.imshow("result", result)
-        if cv2.waitKey(30) & 0xFF == ord("q"):
-            break
-    else:
-        break
-
-
-video.release()
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+# video = cv2.VideoCapture(0)
+#
+#
+# while video.isOpened():
+#     check, frame = video.read()
+#     if frame is not None:
+#         # img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#         # img = frame
+#         low_blue = np.array([110, 20, 0])
+#         upper_blue = np.array([55, 255, 255])
+#         mask_img = cv2.inRange(img, low_blue, upper_blue)
+#         result = cv2.bitwise_and(img, img, mask=mask_img)
+#         # img = frame
+#         cv2.imshow("mask", mask_img)
+#         cv2.imshow("original", img)
+#         cv2.imshow("result", result)
+#         if cv2.waitKey(30) & 0xFF == ord("q"):
+#             break
+#     else:
+#         break
+#
+#
+# video.release()
+# cv2.destroyAllWindows()
+# cv2.waitKey(1)
