23f61afc3a700575d069e839a4b3c839178378c0
fistadev <fabiofistarol@gmail.com>
2021-06-16 22:15:55 +0200

    Create face_eye.py

---
+++ M6 - Computer Vision/CV_projects/face_eye_detection/face_eye.py
@@ -0,0 +1,42 @@
+import cv2
+import time
+import numpy as np
+
+# load data
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye = cv2.CascadeClassifier('haarcascade_eye.xml')
+# font = cv2.FONT_HERSHEY_DUPLEX
+# time_text = str(time.strftime("%H:%M %p"))
+# image = ps.putBText(image,time_text,text_offset_x=image.shape[1]-170,text_offset_y=20,vspace=10,hspace=10, font_scale=1.0,background_RGB=(228,225,222),text_RGB=(1,1,1))
+
+#capture video
+cap_video = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap_video.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(gray, 1.6, 10)
+
+    count_faces = 0
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        rect_faces = gray[y:y + h, x:x + w]
+        rect_eyes = img[y:y + h, x:x + w]
+        eyes = eye.detectMultiScale(rect_faces)
+        # if faces > 1:
+        #     count_faces += 1
+        #     print(len(count_faces))
+
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(rect_eyes, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+
+    # exit videoCapture
+    if k == 27:
+        break
+
+
+cap_video.release()
+cv2.destroyAllWindows()
