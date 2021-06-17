6202b54f52cf02256b38086be4fbd0f0982560d5
fistadev <fabiofistarol@gmail.com>
2021-06-17 17:16:47 +0200

    Update sudoku

--- M6 - Computer Vision/CV_projects/sudoku/sudoku.py
+++ M6 - Computer Vision/CV_projects/sudoku/sudoku.py
@@ -1,3 +1,79 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# print(f'OpenCV: {cv2.__version__}')
+
+
+def imshow(img, enlarge = True, color = True):
+    if enlarge:
+        plt.figure(figsize=(15,10));
+    if not color:
+        plt.imshow(img, cmap='gray');
+    else:
+        plt.imshow(img[:,:,::-1]);
+    plt.show()
+
+sudoku = cv2.imread('img/sudoku.png')
+sudoku_1 = cv2.imread('img/sudoku_1.png',0)
+sudoku_ph = cv2.imread('img/sudoku-photo-2.jpg')
+sudoku_ph_1 = cv2.imread('img/sudoku_1.jpg',0)
+
+
+imshow(sudoku, False)
+
+imshow(sudoku_ph, False)
+
+
+# Create a rotation matrix to rotate the image by 45 degrees (using the center as the pivot point)
+h, w = sudoku_ph.shape[:2]
+
+x_center = w//2
+y_center = h//2
+rotation_matrix = cv2.getRotationMatrix2D((x_center,y_center), -15, 1,)
+
+img_rot = cv2.warpAffine(sudoku_ph,rotation_matrix,(w,h))
+imshow(img_rot)
+
+
+# cropping image
+
+cropped_sudoku = img_rot[110:620,180:810]
+imshow(cropped_sudoku, False, False)
+
+# threshold
+cropped_sudoku_gray = cv2.cvtColor(cropped_sudoku,cv2.COLOR_BGR2GRAY)
+ret, thr =cv2.threshold(cropped_sudoku_gray,127,255,cv2.THRESH_BINARY_INV)
+# imshow(thr)
+
+# canny
+canny = cv2.Canny(cropped_sudoku_gray,100,200)
+# imshow(canny)
+
+# houghlines
+lines = cv2.HoughLines(canny,1,np.pi/180,200)
+# print(lines)
+
+
+sudoku_copy = cropped_sudoku.copy()
+for line in lines:
+    for rho,theta in line:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(sudoku_copy,(x1,y1),(x2,y2),(0,255,0),2)
+
+imshow(sudoku_copy)
+
+
+# solving Sudoku
+
 def findNextCellToFill(grid, i, j):
     for x in range(i, 9):
         for y in range(j, 9):
