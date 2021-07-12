f5f4e1fcd1d4b2339ee607595b47f4ad3b50e2be
fistadev <fabiofistarol@gmail.com>
2021-07-12 13:42:01 +0200

    Update

--- M8 - FullStack/D6 - Postgres/project/app.py
+++ M8 - FullStack/D6 - Postgres/project/app.py
@@ -1,18 +1,24 @@
-from utils.db.connect import HOST
+from utils.db.connect import db_engine
+
+engine = db_engine()
+
+print(engine)
 
 # from utils.db.connect import db_engine
 # from utils.db.models.user import User
 # from sqlalchemy.orm import Session
-print(HOST)
+
 # # db instance
 # engine = db_engine()
 
 # # session instance
-# session = Session(engine,future=True)
+# session = Session(engine, future=True)
 
 
-# def create_user(name,last_name,email,password_hash):
-#     user = User(name=name,last_name=last_name,email=email,password_hash=password_hash)
+# def create_user(name, last_name, email, password_hash):
+#     user = User(
+#         name=name, last_name=last_name, email=email, password_hash=password_hash
+#     )
 #     session.add(user)
 #     session.commit()
 #     return user.toJSON()
@@ -22,18 +28,19 @@ print(HOST)
 #     result = [row.toJSON() for row in session.query(User)]
 #     return result
 
+
 # def get_users_by_id(id):
 #     user = session.query(User).get(id)
-#     if(user):
+#     if user:
 #         return user.toJSON()
 #     return None
 
 
-# def update_user_by_id(id,update):
+# def update_user_by_id(id, update):
 #     user = session.query(User).get(id)
-#     if(user):
-#         for key,value in update.items():
-#             setattr(user,key,value)
+#     if user:
+#         for key, value in update.items():
+#             setattr(user, key, value)
 #         session.commit()
 #         return user.toJSON()
 #     return None
@@ -41,7 +48,7 @@ print(HOST)
 
 # def delete_user_by_id(id):
 #     user = session.query(User).get(id)
-#     if(user):
+#     if user:
 #         session.delete(user)
 #         session.commit()
 #         return user.toJSON()
--- M8 - FullStack/D6 - Postgres/project/utils/db/connect.py
+++ M8 - FullStack/D6 - Postgres/project/utils/db/connect.py
@@ -1,6 +1,5 @@
 from dotenv import dotenv_values
-
-# from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 
 config = dotenv_values(".env")
 
@@ -11,8 +10,8 @@ PASSWORD = config.get("PASSWORD")
 PORT = config.get("PORT")
 
 
-# def db_engine():
-#     ##    "postgresql://USER:PASSWORD@HOST:PORT/DBNAME"
-#     URL = "postgresql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DB)
-#     engine = create_engine(URL, echo=True, hide_parameters=True)
-#     return engine
+def db_engine():
+    ##    "postgresql://USER:PASSWORD@HOST:PORT/DBNAME"
+    URL = "postgresql://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, PORT, DB)
+    engine = create_engine(URL, echo=True, hide_parameters=True)
+    return engine
